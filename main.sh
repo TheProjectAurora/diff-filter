c189300a5674b6ba12c005d706897de055fbd60a
Jani Mikkonen <jani.mikkonen@gmail.com>
2024-01-24 16:26:31 +0200

    moar

--- main.sh
+++ main.sh
@@ -7,24 +7,30 @@ echo ---
 git --no-pager log --decorate=short --pretty=oneline
 echo ---
 
-INPUT_BASE_REF=$1
-INPUT_HEAD_REF=$2
-INPUT_BASE_DIR=$3
-INPUT_DIFF_FILTER=$4
+INPUT_BASE_DIR=$1
+INPUT_DIFF_FILTER=$2
 
-echo DEBUG BASE_REF   : ${INPUT_BASE_REF}
-echo DEBUG HEAD_REF   : ${INPUT_HEAD_REF}
-echo DEBUG BASE_DIR   : ${INPUT_BASE_DIR}
-echo DEBUG DIFF_FILTER: ${INPUT_DIFF_FILTER}
+if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
+  BASE_REF=origin/${GITHUB_BASE_REF}
+  HEAD_REF=HEAD^
+elif  [ "${GITHUB_EVENT_NAME}" == "commit" ]; then
+  BASE_REF=HEAD
+  HEAD_REF=HEAD^
+fi
+
+echo DEBUG BASE_REF   : "${BASE_REF}"
+echo DEBUG HEAD_REF   : "${HEAD_REF}"
+echo DEBUG BASE_DIR   : "${INPUT_BASE_DIR}"
+echo DEBUG DIFF_FILTER: "${INPUT_DIFF_FILTER}"
 
 
 AOUTPUT_TMP=$(
   git diff \
     --name-only \
-    --diff-filter=${INPUT_DIFF_FILTER} \
-    ${INPUT_BASE_REF} \
-    ${INPUT_HEAD_REF} | \
-  grep ^${INPUT_BASE_DIR} | \
+    --diff-filter="${INPUT_DIFF_FILTER}" \
+    "${BASE_REF}" \
+    "${HEAD_REF}" | \
+  grep ^"${INPUT_BASE_DIR}" | \
   xargs -n 1 dirname | \
   awk -F/ '{print $2}' | \
   sort | \
@@ -33,5 +39,5 @@ AOUTPUT_TMP=$(
   jq --slurp . | \
   tr -d "\ \n\r"
 )
-echo DEBUG: ${AOUTPUT_TMP}
-echo "dirs=${AOUTPUT_TMP}">>${GITHUB_OUTPUT}
+echo "DEBUG: ${AOUTPUT_TMP}"
+echo "dirs=${AOUTPUT_TMP}">>"${GITHUB_OUTPUT}"
